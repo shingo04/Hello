8d366fc973d09b8a4cce91ad287fa56eaf925a0d
shingo04 <shingobb04@gmail.com>
2021-03-13 17:09:17 +0900

    変数（数値）

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,7 +6,9 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 23;
+            int number = 8;
+            Console.WriteLine("{0}歳の人は{1}人います", age, number);
         }
     }
 }
